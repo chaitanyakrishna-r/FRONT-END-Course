e4b359547f42c1067632657ef7a2b88de820f1aa
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-10-21 22:06:37 +0530

    started with javascript

---
+++ JavaScript/Lec-01/jscript.js
@@ -0,0 +1,58 @@
+// let age = prompt("Enter your age: ");
+/*
+let age = 18;
+console.log(age);
+if(age >= 18){
+    console.log("yor are eligble for vote");
+}
+else{
+    console.log("yor are not eligble for vote");
+}
+*/
+// let casey = 'ckr';
+// switch(casey){
+//     case 'ck' : console.log('hello from case 1'); 
+//     break;
+//     case 'ckr': console.log('hello from case 2'); 
+//     break;
+//     case 'c' : console.log('hello from case 3'); 
+//     break;
+//     case 'k': console.log('hello from case 4'); 
+//     break;
+
+//     default : console.log('code ends here');
+// }
+
+// for(let i=5; i>0; i--){
+//     if(i == 3)
+//         continue;
+//     else
+//         console.log(i);
+// }
+
+// let a =5;
+// let i=1;
+// while(i<=10){
+//     console.log(a,' * ',i,' = ',a*i);
+//     i++;
+// }
+
+
+// let a =5;
+// let i=1;
+
+// do{
+//     console.log(a,'*' ,i,' = ',a*i);
+//     i++;
+// }while(i<=10);
+
+//strings
+
+let firstName = 'chaitanya krishna';
+let lastName = "hey how are you ";
+let name = `hello 
+how are you `;
+
+console.log(typeof(lastName));
+console.log(typeof(firstName));
+console.log(typeof(name));
