98304f24cb1c4c0679ad855afe3ae7e762bec50b
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-11-01 14:15:57 +0530

    array and functoins

---
+++ JavaScript/array/index.js
@@ -0,0 +1,57 @@
+// objects
+/*let obj = {
+    name :"chaitanyakrisna",
+    gender : "male",
+    age : "25",
+    greet : function(){
+        console.log("hello",obj.name)
+    } 
+}
+
+console.log(obj);
+console.log(obj.greet());*/
+
+
+// arrays
+
+// let arr = [1,2,3,'ck',8];
+
+// create array using array constructors
+// let arr1 = new Array('ck',2,true);
+
+// console.log(arr1);
+
+// let arr = [1,3,2,5,9,8,6];
+// arr.splice(3,2);
+// console.log(arr);
+
+// map
+
+// let arr = [10, 20, 30];
+
+// arr.map((number)=>{
+//     console.log(number**2);
+// })
+
+// let arr1= [1,2,3,4,5,6,8,9,45,65];
+
+// let evenAns = arr1.filter((num)=>{
+//     let even;
+//     if(num%2 == 0){
+//      even = num;
+//     }
+// })
+// console.log(evenAns);
+
+
+// let suarr=[10,20,30,40];
+// let ans = suarr.reduce((a,b)=>{
+//     return a+b;
+// },10);
+
+// console.log(ans);
+ let arr =[10 ,20,30,40];
+
+ arr.forEach((num,indi)=>{
+    console.log("number : ",num,"index: ",indi);
+ })
