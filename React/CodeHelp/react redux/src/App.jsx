960f9643cdad1355516a6cda84a5b5db11b088e3
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2024-12-15 20:09:08 +0530

    complete redux

--- React/CodeHelp/react redux/src/App.jsx
+++ React/CodeHelp/react redux/src/App.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { decrement, increment,reset,incrementByAmount } from './features/counter/counterSlice';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount ,setAmount] =useState(0);
+  const count = useSelector((state)=>state.counter.value);
+  const dispatch = useDispatch();
+  
+  function handleIncrementClick(){
+    dispatch(increment());
+  }
+  function handleDecrementClick(){
+    dispatch(decrement());
+
+  }
+  function handleReset(){
+    dispatch(reset());
+  }
+  function handleAmountIncrement(){
+    dispatch(incrementByAmount(amount));
+  }
 
   return (
+    
     <>
-      
+      <div className='container'>
+        <div className='wrapper'>
+          <button onClick={handleIncrementClick}>+</button>
+          <p> Count: {count}</p>
+          <button onClick={handleDecrementClick}>-</button>
+        </div>
+        <br />
+        <div>
+          <button onClick={handleReset}>Reset</button>
+          <input type="text" value={amount} placeholder='enter amount'
+          onChange={(e)=>{setAmount(e.target.value)
+            console.log(amount)
+          }} />
+          <button onClick={handleAmountIncrement}>Add</button>
+        </div>
+      </div>
     </>
   )
 }
