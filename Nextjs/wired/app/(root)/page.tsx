c024a7ba7c0b6c91e5853e54f5254fd22cb796fd
Chaitanyakrishna-r <chaitanyakrishna1120@gmail.com>
2025-02-11 21:47:21 +0530

    wired project

---
+++ Nextjs/wired/app/(root)/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    
+      <section className="pink_container">
+         <h1 className="heading">Pitch Your StartUp,<br/> Connect With Entreprenenurs </h1>
+         <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
+         </p>
+        
+      </section>
+      <section className="newPattern bg-slate-600 h-52">
+        <p className="">hellow</p>
+      </section>
+     
+    </>
+  );
+}
